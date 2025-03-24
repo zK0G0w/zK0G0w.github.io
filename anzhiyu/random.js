d6dc3747b96d7abf75f8b3009fc609b97e292cc7
WainZeng <zk0g0w@gmail.com>
2025-03-24 16:11:41 +0800

    Site updated: 2025-03-24 16:11:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/14/hello-world/"];function toRandomPost(){
+var posts=["2025/03/24/test/","2025/03/24/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
